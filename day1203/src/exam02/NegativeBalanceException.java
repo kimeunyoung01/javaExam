633d00e07ed5e6e31239090c978ba397e32192e8
4000 <rola70@nate.com>
2018-12-03 11:45:43 +0900

    first commit

---
+++ day1203/src/exam02/NegativeBalanceException.java
@@ -0,0 +1,10 @@
+package exam02;
+
+public class NegativeBalanceException extends Exception {
+
+	public NegativeBalanceException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ day1203/src/exam02/Person.java
@@ -0,0 +1,87 @@
+package exam02;
+
+public class Person {
+	private String name;
+	private int age;
+	private String addr;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public Person(String name, int age, String addr) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.addr = addr;
+	}
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", addr=" + addr + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((addr == null) ? 0 : addr.hashCode());
+		result = prime * result + age;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (addr == null) {
+			if (other.addr != null)
+				return false;
+		} else if (!addr.equals(other.addr))
+			return false;
+		if (age != other.age)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
